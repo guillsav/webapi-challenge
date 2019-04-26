2641f56eb0c0eceaee01db4fa8da5b7e00650f5b
guillsav <guillsav@gmail.com>
2019-04-26 11:28:18 -0400

    Server and routers setup

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const projectsRouter = require('./projects/projects-router.js');
+const actionsRouter = require('./actions/actions-router.js');
+
+const server = express();
+
+server.use('/api/projects', projectsRouter);
+server.use('/api/actions', actionsRouter);
+
+module.exports = server;
