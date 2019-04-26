5385e7ab84ea7a0eadd0f4d8f4975b5ceb5d3573
guillsav <guillsav@gmail.com>
2019-04-26 11:29:30 -0400

    express.json middleware added

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const actionsRouter = require('./actions/actions-router.js');
 
 const server = express();
 
+server.use(express.json());
 server.use('/api/projects', projectsRouter);
 server.use('/api/actions', actionsRouter);
 
